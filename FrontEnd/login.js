2a19948f90deffd86a89b9ccefa9f22bf2a69e61
Julia Taylor <julia.taylor3@gmail.com>
2023-03-16 00:03:16 +0100

    login partiellement fonctionnel.

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -3,18 +3,24 @@
 
 
 const loginForm=document.querySelector(".login-form");
-loginForm.addEventListener("submit",function(e){
+loginForm.addEventListener("submit", async function(e){
     e.preventDefault();
     let user={
         email:document.querySelector("[name=email]").value,
         password: document.querySelector("[name=password]").value,} 
     console.log(user,'user');
-    fetch('http://localhost:5678/api/users/login',{
-        "method":'POST',
-        "headers":{"Content-Type" : "application/json"},
-        "body": JSON.stringify(user),
-    });}
-.then(response=>response.json()))
-.then(json=>console.log(json))
-.catch(error=>console.log(error))
+    async function fetchUsers (){
+        const r =await fetch('http://localhost:5678/api/users/login',{
+            "method":'POST',
+            "headers":{"Content-Type" : "application/json"},
+            "body": JSON.stringify(user),
+        });
+        if (r.ok ===true){
+            return r.json();
+        }
+        throw new Error('Erreur dans l’identifiant ou le mot de passe');
+    }
+    fetchUsers().then(users=>console.log(users))
+})
+
 
