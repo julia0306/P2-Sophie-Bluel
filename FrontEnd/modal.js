1dfa60a91bf23fc8f51cc6d90e83391fbdf9fca1
Julia Taylor <julia.taylor3@gmail.com>
2023-03-19 16:01:56 +0100

    modification des ouvertures / fermetures de la modale 'Galerie Photo'

--- FrontEnd/modal.js
+++ FrontEnd/modal.js
@@ -3,15 +3,14 @@ const editorContent=document.querySelectorAll('.editor-content');
 const logoutButton=document.getElementById('logout-btn');
 const loginButton=document.getElementById('login-btn');
 const filterBar=document.querySelector('.filterbar')
-const editGalleryButton=document.querySelector('.edit-link-b');
-const modalContainer=document.getElementById('modal-container');
+const editGalleryButton=document.querySelector('.js-modal');
 const modalGallery=document.querySelector('.modal-gallery');
-const icon = document.querySelector('.fa-times');
-const maximizeIcon =modalContainer.querySelectorAll('.fa-arrows-up-down-left-right');
-console.log(maximizeIcon, "MI")
 const trashIcon = document.querySelector('.fa-trash-bin');
+const modalContainer=document.getElementById('modal-container');
+const maximizeIcon =modalContainer.querySelectorAll('.fa-arrows-up-down-left-right');
+console.log(maximizeIcon, "MI");
 const userToken=localStorage.getItem('token');
-const modalWrapper=document.querySelector('.modal-wrapper');
+
 
 
 // Je récupère les travaux depuis l'API et je crée ma modale
@@ -65,28 +64,46 @@ logoutButton.addEventListener('click', function(){
     localStorage.clear('token');
     loginButton.style.display="block";
     logoutButton.style.display="none";
-    modalContainer.style.display="none";
 })
-
 
 // J'affiche la modale lorsque l'utilisateur clique sur le bouton "Modifier'
+let modal = null
 
-editGalleryButton.addEventListener('click', function(e){
-    e.preventDefault;
-    modalWrapper.style.visibility="visible"
+const openModal = function(e){
+    e.preventDefault()
+    const target = document.querySelector(e.target.getAttribute('href'))
+    target.style.display=null
+    target.removeAttribute('aria-hidden')
+    target.setAttribute('aria-modal', 'true')
+    modal=target
+    modal.addEventListener('click', closeModal)
+    modal.querySelector('.fa-times').addEventListener('click', closeModal)
+    modal.querySelector('.js-modal-stop').addEventListener('click', stopPropagation)
+}
+// Je supprime la modale lorsque l'utilisateur clique sur la croix ou sur la fenêtre modale
+const closeModal = function(e){
+    if (modal === null) return
+    e.preventDefault()
+    modal.style.display="none"
+    modal.setAttribute('aria-hidden', 'true')
+    modal.removeAttribute('aria-modal')
+    modal.removeEventListener('click', closeModal)
+    modal.querySelector('.fa-times').removeEventListener('click', closeModal)
+    modal.querySelector('.js-modal-stop').removeEventListener('click', stopPropagation)
+    modal = null
+}
+// J'empêche la modale de se refermer lorsque l'utilisateur clique sur le contenu de la modale
+const stopPropagation = function (e) {
+    e.stopPropagation()
+}
+
+document.querySelectorAll('.js-modal').forEach(a =>{a.addEventListener('click', openModal)
+
+})
+// J'instaure la fermeture de la modale au clic de la touche "Echap"
+window.addEventListener('keydown', function(e){
+    if (e.key === 'Escape' || e.key === "Esc"){closeModal(e)}
 })
 
-// Je cache la modale lorsque l'utilisateur clique sur l'icône "x"
-
-icon.addEventListener('click', function(e){
-    e.preventDefault;
-    modalWrapper.style.visibility="hidden"}
-)
-
-//Ou bien s'il clique ailleurs sur la page
-modalWrapper.addEventListener('click', function(e){
-    e.preventDefault;
-    modalWrapper.style.visibility='hidden'
-})
 
 
